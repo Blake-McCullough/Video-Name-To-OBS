41d21e794fbe1eb1fbd71bee08cd9faa2af2a6c1
Blake-McCullough <66660752+Blake-McCullough@users.noreply.github.com>
2022-06-20 17:00:00 +0800

    Add files via upload

---
+++ Chrome--Plugin/background.js
@@ -0,0 +1,34 @@
+// function that injects code to a specific tab
+function injectScript(tabId) {
+
+    chrome.scripting.executeScript(
+        {
+            target: { tabId: tabId },
+            files: ['inject.js'],
+        }
+    );
+
+}
+function sended(id) {
+    try {
+        if (id.startsWith("https://music.youtube.com/")) {
+            url = 'http://localhost/fetchvid?id=' + id;
+            fetch(url);
+            
+
+        }
+    } catch { console.log("An error occured :Awkward:") }
+}
+
+// adds a listener to tab change
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+
+    // check for a URL in the changeInfo parameter (url is only added when it is changed)
+    if (changeInfo.url) {
+
+
+        // calls the inject function
+        sended(changeInfo.url);
+
+    }
+});
